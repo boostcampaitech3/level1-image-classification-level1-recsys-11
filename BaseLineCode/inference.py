d284ff95e8596f62b9502fc7d23a417786d72684
Gyeongtae <igt0530@gmail.com>
2022-02-24 02:05:31 +0000

    update 02 24

--- BaseLineCode/inference.py
+++ BaseLineCode/inference.py
@@ -45,7 +45,7 @@ def inference(data_dir, model_dir, output_dir, args):
     loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=args.batch_size,
-        num_workers=8,
+        # num_workers=4, #8,
         shuffle=False,
         pin_memory=use_cuda,
         drop_last=False,
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 
     # Data and model checkpoints directories
     parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--resize', type=tuple, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
+    parser.add_argument('--resize', nargs="+",type=int, default=[96, 128], help='resize size for image when you trained (default: (96, 128))')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
 
     # Container environment
@@ -83,7 +83,6 @@ if __name__ == '__main__':
     data_dir = args.data_dir
     model_dir = args.model_dir
     output_dir = args.output_dir
-
     os.makedirs(output_dir, exist_ok=True)
 
     inference(data_dir, model_dir, output_dir, args)
