77c35b8bc4adce852349f1ba2dcbe720a6a7cb1a
kidjung <kjpark4321@naver.com>
2022-02-28 06:21:14 +0000

    [feat] implement single model inference function

--- BaseLineCodeV2/inference.py
+++ BaseLineCodeV2/inference.py
@@ -8,6 +8,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from dataset import TestDataset, MaskBaseDataset
+import dataset
 
 
 def load_model(saved_model, num_classes, device):
@@ -72,6 +73,59 @@ def inference(data_dir, model_dir, output_dir, args):
     print(f"Inference Done! Inference result saved at {save_path}")
 
 
+def inference_by_single_models(which_model, data_dir, model_dir, output_dir, args):
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # which_model should be 'mask' or 'genderAge'
+    if which_model == 'mask':
+        num_classes = 3
+    elif which_model == 'genderAge':
+        num_classes = 6
+    else:
+        assert 'no match model type'
+
+    model = load_model(model_dir, num_classes, device).to(device)
+    model.eval() 
+
+    img_root = os.path.join(data_dir, 'images')
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
+
+    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+
+
+    # -- dataset
+    dataset_module = getattr(import_module("dataset"), args.dataset)  # default: TestDataset
+    dataset = dataset_module(img_paths, args.resize)
+    # dataset = TestDataset(img_paths, args.resize)
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=False,
+    )
+
+    print("Calculating inference results.." + which_model)
+    preds = []
+    with torch.no_grad():
+        for idx, images in enumerate(loader):
+            images = images.to(device)
+            pred = model(images)
+            pred = pred.argmax(dim=-1)
+            preds.extend(pred.cpu().numpy())
+
+    info['ans'] = preds
+    save_path = os.path.join(output_dir, f'output' + which_model + '.csv')
+    info.to_csv(save_path, index=False)
+
+def inference_ensemble():
+    inference_by_single_models('mask')
+    inference_by_single_models('genderAge')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -80,6 +134,7 @@ if __name__ == '__main__':
     parser.add_argument('--resize', nargs="+", type=int, default=[96, 128], help='resize size for image when you trained (default: (96, 128))')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
     parser.add_argument('--dataset', type=str, default='TestDataset', help='TestDataset with data augmentation  (default: TestDataset)')
+    parser.add_argument('--isEnsemble', type=str, default='False', help='Inference tpye True / False  (default: False)')
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
