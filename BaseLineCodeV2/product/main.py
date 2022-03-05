b96fc2502fa6760648cec50e1820e8a2e81b3dc5
Gyeongtae <igt0530@gmail.com>
2022-03-05 14:26:09 +0000

    update last

--- BaseLineCodeV2/product/main.py
+++ BaseLineCodeV2/product/main.py
@@ -1,11 +1,75 @@
-import imp
+import os
 import flask
 from flask import Flask, request, render_template
-from sklearn.externals import joblib
-import numpy as np
-from scipy import misc
 
-from BaseLineCodeV2.inference import load_model
+import math
+import numpy as np
+
+import imageio as iio
+from PIL import Image
+
+import torch
+from torchvision import models
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.transforms import Resize, ToTensor, Normalize, Compose, CenterCrop, InterpolationMode
+
+# input image process
+# resize = (300, 300)
+mean=(0.548, 0.504, 0.479)
+std=(0.237, 0.247, 0.246)
+transforms = Compose([
+            CenterCrop(360),
+            Resize(300, InterpolationMode), #, Image.BILINEAR            
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+        ])
+
+
+# model load
+class EfficientNetB3MSD(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.dropout_p = 0.5
+        self.dropout_n = 5
+        self.efficientnet = models.efficientnet_b3(pretrained=False)
+
+        self.dropouts = nn.ModuleList([
+            nn.Dropout(self.dropout_p) for _ in range(self.dropout_n)
+        ])
+        
+        in_features = self.efficientnet.classifier[1].out_features
+        self.linear = nn.Linear(in_features=in_features, out_features=num_classes)
+
+        # initialize w & b
+        torch.nn.init.xavier_uniform_(self.linear.weight)
+        stdv = 1 / math.sqrt(self.linear.in_features)
+        self.linear.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        x = self.efficientnet(x)
+        for i, dropout in enumerate(self.dropouts):
+            if i == 0:
+                h = self.linear(dropout(x))
+            else:
+                h += self.linear(dropout(x))
+        output = h / len(self.dropouts)
+        return output
+
+model = EfficientNetB3MSD(18)
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+model_path = os.path.join('../model/general/FocalEfficientNetB3MSD', 'best.pth' )
+model.load_state_dict(torch.load(model_path, map_location=device))
+
+# label checking 
+Mask_type   = ['Wear', 'Incorrect', 'Not Wear']
+Gender_type = ['Male', 'Female']
+Age_type    = ['<30', '>=30 and <60', '>=60']
+
+class_description = {i*6+j*3+k : f"{mask} & {gender} & {age}"  for i, mask in enumerate(Mask_type) for j, gender in enumerate(Gender_type) for k, age in enumerate(Age_type)}
+
 
 app = Flask(__name__)
 
@@ -25,16 +89,21 @@ def make_prediction():
         if not file : return render_template('index.html', label = 'No Files')
 
         # 이미지 픽셀 정보 읽기
-        img = misc.imread(file)
-        img = img[:, :, :3]
-        img = img.reshape(1,-1)
-
-        load_model
+        img = iio.imread(file)
+        image = transforms(img)    
 
         # prediction = model.predict(img)
         model.eval()
-        pred = model(img)
+        pred = model(image)
         pred = pred.argmax(dim=-1)
         pred = pred.cpu().numpy()
+
+        label = class_description[pred]
+
+        return render_template('index.html', label = label)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=30010, debug= True)
+
 
 
--- BaseLineCodeV2/train.py
+++ BaseLineCodeV2/train.py
@@ -339,7 +339,7 @@ if __name__ == '__main__':
     parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
     parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help='resize size for image when training')
     parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
-    parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--valid_batch_size', type=int, default=422, help='input batch size for validing (default: 422)')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
     parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer type (default: SGD)')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
